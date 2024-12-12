ceb6b8bc003bcffc3733d8339a5cb7756f6588f7
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-12-12 23:58:29 +0100

    Update NASS_Vectors.cpp
    Norms

--- src/NASS_Vectors.cpp
+++ src/NASS_Vectors.cpp
@@ -82,14 +82,109 @@ namespace nass {
 
             #pragma omp parallel for reduction(+: R0, R1)
             for(natural_t N1 = 0; N1 < N0 - 3; N1 += 4) {
-                R0 += Rv0[N1 + 0] * Rv1[N1 + 0]; // Superfluous.
+                R0 += Rv0[N1] * Rv1[N1];
                 R1 += Rv0[N1 + 1] * Rv1[N1 + 1];
             }
 
             for(natural_t N1 = N0 - (N0 % 2); N1 < N0; ++N1)
-                R0 += Rv0[N1 + 0] * Rv1[N1 + 0];
+                R0 += Rv0[N1] * Rv1[N1];
 
             return R0 + R1;
+
+            #endif
+        }
+
+
+        /**
+         * @brief Norm of a real_t*.
+         * 
+         * @param Rv0 Real vector [Rv].
+         * @param N0 Natural number [N].
+         * @return real_t 
+         */
+        real_t Nr_RvN_R(const real_t* Rv0, const natural_t& N0) {
+            #ifdef __ARM_NEON
+            
+            reals_t Rs0 = Ex_R_Rs(0.0), Rs1 = Ex_R_Rs(0.0);
+            
+            #pragma omp parallel for reduction(Rd_Rs: Rs0, Rs1)
+            for(natural_t N1 = 0; N1 < N0 - LOOP_OFFSET + 1; N1 += LOOP_OFFSET) {
+                const reals_t Rs2 = Ld_Rv_Rs(Rv0 + N1 + MEMORY_OFFSET_0);
+                const reals_t Rs3 = Ld_Rv_Rs(Rv0 + N1 + MEMORY_OFFSET_1);
+
+                Rs0 = Ad_RsRs_Rs(Rs0, Ml_RsRs_Rs(Rs2, Rs2));
+                Rs1 = Ad_RsRs_Rs(Rs1, Ml_RsRs_Rs(Rs3, Rs3));
+            }
+
+            real_t R0 = Rd_Rs_R(Rs0) + Rd_Rs_R(Rs1);
+
+            for(natural_t N1 = N0 - (N0 % LOOP_OFFSET); N1 < N0; ++N1) {
+                const real_t R3 = Rv0[N1];
+
+                R0 += R3 * R3;
+            }
+
+            return std::sqrt(R0);
+
+            #else
+
+            real_t R0 = 0.0, R1 = 0.0;
+
+            #pragma omp parallel for reduction(+: R0, R1)
+            for(natural_t N1 = 0; N1 < N0 - 3; N1 += 4) {
+                const real_t R3 = Rv0[N1], R4 = Rv0[N1 + 1];
+
+                R0 += R3 * R3;
+                R1 += R4 * R4;
+            }
+
+            for(natural_t N1 = N0 - (N0 % 2); N1 < N0; ++N1) {
+                const real_t R3 = Rv0[N1];
+
+                R0 += R3 * R3;
+            }
+
+            return std::sqrt(R0 + R1);
+
+            #endif
+        }
+
+
+        /**
+         * @brief Normalization of a real_t*.
+         * 
+         * @param Rvt0 Real vector [Rv], target [t].
+         * @param N0 Natural number [N].
+         */
+        void Nrz_RvN_0(real_t* Rvt0, const natural_t& N0) {
+            const real_t R0 = Nr_RvN_R(Rvt0, N0);
+
+            #ifdef __ARM_NEON
+            
+            const reals_t Rs0 = Ex_R_Rs(R0);
+            
+            #pragma omp parallel for
+            for(natural_t N1 = 0; N1 < N0 - LOOP_OFFSET + 1; N1 += LOOP_OFFSET) {
+                const reals_t Rs1 = Ld_Rv_Rs(Rvt0 + N1 + MEMORY_OFFSET_0);
+                const reals_t Rs2 = Ld_Rv_Rs(Rvt0 + N1 + MEMORY_OFFSET_1);
+
+                St_RvtRs_0(Rvt0 + N1 + MEMORY_OFFSET_0, Dv_RsRs_Rs(Rs1, Rs0));
+                St_RvtRs_0(Rvt0 + N1 + MEMORY_OFFSET_1, Dv_RsRs_Rs(Rs2, Rs0));
+            }
+
+            for(natural_t N1 = N0 - (N0 % LOOP_OFFSET); N1 < N0; ++N1)
+                Rvt0[N1] /= R0;
+
+            #else
+
+            #pragma omp parallel for reduction(+: R0, R1)
+            for(natural_t N1 = 0; N1 < N0 - 3; N1 += 4) {
+                Rvt0[N1] /= R0;
+                Rvt0[N1 + 1] /= R0;
+            }
+
+            for(natural_t N1 = N0 - (N0 % 2); N1 < N0; ++N1)
+                Rvt0[N1] /= R0;
 
             #endif
         }
