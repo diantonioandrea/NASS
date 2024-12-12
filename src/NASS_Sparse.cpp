798adca82cd01b9b4889fe1f44c39774fe29d5bf
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-12-12 21:23:59 +0100

    Update NASS_Sparse.cpp

--- src/NASS_Sparse.cpp
+++ src/NASS_Sparse.cpp
@@ -23,7 +23,7 @@ namespace nass {
          * @param St String [St]
          * @return std::tuple<natural_t, natural_t*, natural_t*, real_t*> (CSC) sparse matrix [Spc].
          */
-        std::tuple<natural_t, natural_t*, natural_t*, real_t*> Spc_St_NNvNvRv(const std::string& St0) {
+        [[nodiscard]] std::tuple<natural_t, natural_t*, natural_t*, real_t*> Spc_St_NNvNvRv(const std::string& St0) {
             natural_t N0, N1, N2, N3 = 0, N4 = 0;
             real_t R0;
             
@@ -79,6 +79,107 @@ namespace nass {
                 Nv0[N3 + 1] = N4;
 
             return {N0, Nv0, Nv1, Rv0};
+        }
+
+
+        /**
+         * @brief Loads a (CSR) sparse matrix from a file.
+         * 
+         * @param St String [St]
+         * @return std::tuple<natural_t, natural_t*, natural_t*, real_t*> (CSC) sparse matrix [Spc].
+         */
+        [[nodiscard]] std::tuple<natural_t, natural_t*, natural_t*, real_t*> Spr_St_NNvNvRv(const std::string& St0) {
+            natural_t N0, N1, N2, N3 = 0, N4 = 0;
+            real_t R0;
+            
+            // Line and file.
+            std::string St1;
+            std::ifstream F0(St0);
+
+            // First line.
+            std::getline(F0, St1);
+
+            // First line stream.
+            std::istringstream Ss0(St1);
+
+            // Parameters.
+            Ss0 >> N0; // Rows.
+            Ss0 >> N1; // Columns.
+            Ss0 >> N2; // Entries.
+
+            // Square check.
+            assert(N0 == N1);
+
+            // Initialization.
+            natural_t* Nv0 = new natural_t[N0 + 1];
+            natural_t* Nv1 = new natural_t[N2];
+            real_t* Rv0 = new real_t[N2];
+
+            // Reading.
+            while(std::getline(F0, St1)) {
+
+                // Line stream.
+                std::istringstream Ss1(St1);
+
+                // Coordinates and entry.
+                Ss1 >> N1; --N1; // Row.
+                Ss1 >> N2; --N2; // Column.
+                Ss1 >> R0; // Entry.
+
+                // Sparse check.
+                if(std::abs(R0) < real_tol)
+                    continue;
+
+                for(; N3 < N1; ++N3)
+                    Nv0[N3 + 1] = N4;
+
+                Nv1[N4] = N2;
+                Rv0[N4] = R0;
+
+                ++N4;
+            }
+
+            // Last entries.
+            for(; N3 < N0; ++N3)
+                Nv0[N3 + 1] = N4;
+
+            return {N0, Nv0, Nv1, Rv0};
+        }
+
+
+        /**
+         * @brief Multiplies a (CSC) sparse matrix by a real_t*.
+         * 
+         * @param Rvt0 Real vector [Rv], target [t].
+         * @param N0 Natural number [N].
+         * @param Nv0 Natural vector [Nv].
+         * @param Nv1 Natural vector [Nv].
+         * @param Rv0 Real vector [Rv].
+         * @param Rv1 Real vector [Rv].
+         */
+        void Mlc_RvtNNvNvRvRv_0(real_t* Rvt0, const natural_t& N0, const natural_t* Nv0, const natural_t* Nv1, const real_t* Rv0, const real_t* Rv1) {
+            for(natural_t N1 = 0; N1 < N0; ++N1)
+                for(natural_t N2 = Nv0[N1]; N2 < Nv0[N1 + 1]; ++N2)
+                    Rvt0[Nv1[N2]] += Rv0[N2] * Rv1[N1];
+        }
+
+
+        /**
+         * @brief Multiplies a (CSR) sparse matrix by a real_t*.
+         * 
+         * @param Rvt0 Real vector [Rv], target [t].
+         * @param N0 Natural number [N].
+         * @param Nv0 Natural vector [Nv].
+         * @param Nv1 Natural vector [Nv].
+         * @param Rv0 Real vector [Rv].
+         * @param Rv1 Real vector [Rv].
+         */
+        void Mlr_RvtNNvNvRvRv_0(real_t* Rvt0, const natural_t& N0, const natural_t* Nv0, const natural_t* Nv1, const real_t* Rv0, const real_t* Rv1) {
+
+            #pragma omp parallel for
+            for(natural_t N1 = 0; N1 < N0; ++N1)
+                for(natural_t N2 = Nv0[N1]; N2 < Nv0[N1 + 1]; ++N2)
+                    Rvt0[N1] += Rv0[N2] * Rv1[Nv1[N2]];
         }
 
     }
