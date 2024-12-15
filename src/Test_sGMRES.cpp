a1169467fc532f9aa6f1f2cadb3ffd2ee5182239
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-12-15 21:57:19 +0100

    Update Test_sGMRES.cpp

--- src/Test_sGMRES.cpp
+++ src/Test_sGMRES.cpp
@@ -52,17 +52,18 @@ int main(int argc, char** argv) {
     internal::Mlc_RvtNNvNvRvRv_0(Rv3, N0, Nv0, Nv1, Rv0, Rv2);
 
     // sGMRES.
-    const real_t R0 = internal::sGMRES_RvNNvNvRvRvNN_0(Rv1, N0, Nv0, Nv1, Rv0, Rv3, N1, N2);
+    const auto [R0, R1] = internal::sGMRES_RvNNvNvRvRvNN_RR(Rv1, N0, Nv0, Nv1, Rv0, Rv3, N1, N2);
 
     // Residual.
     internal::RMlc_RvtNNvNvRvRvRv_0(Rv4, N0, Nv0, Nv1, Rv0, Rv1, Rv3);
 
     // Relative residual.
-    const real_t R1 = internal::Nr_RvN_R(Rv4, N0) / internal::Nr_RvN_R(Rv3, N0);
+    const real_t R2 = internal::Nr_RvN_R(Rv4, N0) / internal::Nr_RvN_R(Rv3, N0);
 
     // Output.
-    std::println("Residual, estimate: {:.3e}", R0);
-    std::println("Residual, relative: {:.3e}", R1);
+    std::println("Residual, estimate:  {:.3e}", R0);
+    std::println("Condition, estimate: {:.3e}\n", R1);
+    std::println("Residual, relative:  {:.3e}", R2);
 
     // Clean-up.
     delete[] Nv0; delete[] Nv1; delete[] Rv0;
