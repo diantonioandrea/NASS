e47b3616e249d59e8b4e4cc4669056fc8711f06b
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-12-19 17:09:42 +0100

    Update NASS_Solver.cpp
    Memory priority version

--- src/NASS_Solver.cpp
+++ src/NASS_Solver.cpp
@@ -12,8 +12,16 @@
 #define SPARSE_SKETCH
 #endif
 
+#if defined(GAUSS_SKETCH) && defined(MEMORY_PRIORITY)
+#error "Clashing options."
+#endif
+
 #ifndef NVERBOSE
 #include <chrono>
+#endif
+
+#ifndef NDEBUG
+#include <cassert>
 #endif
 
 #include "../include/Vectors.hpp"
@@ -39,9 +47,18 @@ namespace nass {
          * @return std::array<real_t, 2> Real numbers [R].
          */
         std::array<real_t, 2> sGMRES_RvNNvNvRvRvNN_RR(real_t* Rvt0, const natural_t& N0, const natural_t* Nv0, const natural_t* Nv1, const real_t* Rv0, const real_t* Rv1, const natural_t& N1, const natural_t& N2) {
+            #ifndef NDEBUG // Integrity check.
+            assert(N1 > 0);
+            assert(N2 > 0);
+            assert(N2 <= N1);
+            #endif
 
             #ifndef NVERBOSE
+            #ifndef MEMORY_PRIORITY
             std::println("--- sGMRES.");
+            #else
+            std::println("--- sGMRES, Memory priority.");
+            #endif
             std::println("Parameters: {}, {}, {}", N0, N1, N2);
             std::println("Timings:");
             #endif
@@ -69,7 +86,7 @@ namespace nass {
             #ifndef NVERBOSE
             auto T1 = std::chrono::high_resolution_clock::now();
 
-            std::println("\tSketch: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
+            std::println("\tSketch generation: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
             #endif
 
 
@@ -78,13 +95,14 @@ namespace nass {
             #endif
 
 
-            // Basis.
-            real_t* Rm1 = new real_t[N0 * N1];
+            #ifndef MEMORY_PRIORITY
+            real_t* Rm1 = new real_t[N0 * N1]; // Basis.
+            real_t* Rm2 = new real_t[N0 * N1]; // LS matrix.
+            #else 
+            real_t* Rm1 = new real_t[N0 * (N2 + 1)]; // Partial basis.
+            #endif
 
-            // LS matrix.
-            real_t* Rm2 = new real_t[N0 * N1];
-
-            // Sketch matrix.
+            // Sketched LS matrix.
             real_t* Rm3 = new real_t[N3 * N1];
 
             // QR.
@@ -139,54 +157,120 @@ namespace nass {
             Cp_RvtRvN_0(Rm1, Rv4, N0);
             Nrz_RvtN_0(Rm1, N0);
 
+            #ifndef MEMORY_PRIORITY
+
             // First LS column.
             Mlc_RvtNNvNvRvRv_0(Rm2, N0, Nv0, Nv1, Rv0, Rm1);
 
-            // Truncated Arnoldi.
-            const natural_t N4 = N2 > N1 ? N1 : N2;
-
-            // Truncated Arnoldi, first columns.
-            for(natural_t N5 = 1; N5 < N4; ++N5) {
+            // Truncated Arnoldi, first part.
+            for(natural_t N4 = 1; N4 <= N2; ++N4) {
 
                 // Copy.
-                Cp_RvtRvN_0(Rm1 + N5 * N0, Rm2 + (N5 - 1) * N0, N0);
+                Cp_RvtRvN_0(Rm1 + N4 * N0, Rm2 + (N4 - 1) * N0, N0);
 
                 // Orthogonalization.
-                for(natural_t N6 = 1; N6 <= N5; ++N6)
-                    Prj_RvtRvRvN_0(Rm1 + N5 * N0, Rm1 + (N5 - N6) * N0, Rm1 + N5 * N0, N0);
+                for(natural_t N5 = 0; N5 < N4; ++N5)
+                    Prj_RvtRvRvN_0(Rm1 + N4 * N0, Rm1 + N5 * N0, Rm1 + N4 * N0, N0);
+
+                // Normalization.
+                Nrz_RvtN_0(Rm1 + N4 * N0, N0);
+
+                // LS matrix.
+                Mlc_RvtNNvNvRvRv_0(Rm2 + N4 * N0, N0, Nv0, Nv1, Rv0, Rm1 + N4 * N0);
+            }
+
+            // Truncated Arnoldi, second part.
+            for(natural_t N4 = N2 + 1; N4 < N1; ++N4) {
+
+                // Copy.
+                Cp_RvtRvN_0(Rm1 + N4 * N0, Rm2 + (N4 - 1) * N0, N0);
+
+                // Orthogonalization.
+                for(natural_t N5 = N4 - N2; N5 < N4; ++N5)
+                    Prj_RvtRvRvN_0(Rm1 + N4 * N0, Rm1 + N5 * N0, Rm1 + N4 * N0, N0);
+
+                // Normalization.
+                Nrz_RvtN_0(Rm1 + N4 * N0, N0);
+
+                // LS matrix.
+                Mlc_RvtNNvNvRvRv_0(Rm2 + N4 * N0, N0, Nv0, Nv1, Rv0, Rm1 + N4 * N0);
+            }
+
+            #else
+
+            // Truncated Arnoldi and sketching, first part.
+            for(natural_t N4 = 1; N4 <= N2; ++N4) {
+
+                // LS column.
+                Mlc_RvtNNvNvRvRv_0(Rm1 + N4 * N0, N0, Nv0, Nv1, Rv0, Rm1 + (N4 - 1) * N0);
+
+                // Sketch application.
+                Mlc_RvtNNvNvRvRv_0(Rm3 + (N4 - 1) * N3, N0, Nv2, Nv3, Rv2, Rm1 + N4 * N0);
+
+                // Orthogonalization.
+                for(natural_t N5 = 0; N5 < N4; ++N5)
+                    Prj_RvtRvRvN_0(Rm1 + N4 * N0, Rm1 + N5 * N0, Rm1 + N4 * N0, N0);
+
+                // Normalization.
+                Nrz_RvtN_0(Rm1 + N4 * N0, N0);
+            }
+
+            // Truncated Arnoldi and sketching, second part.
+            for(natural_t N4 = N2 + 1; N4 < N1; ++N4) {
+
+                // Indices.
+                const natural_t N5 = N4 % (N2 + 1);
+                const natural_t N6 = (N4 - 1) % (N2 + 1);
+
+                // Zeroing.
+                #pragma omp parallel for
+                for(natural_t N7 = 0; N7 < N0; ++N7)
+                    Rm1[N5 * N0 + N7] = 0.0;
+
+                // LS column.
+                Mlc_RvtNNvNvRvRv_0(Rm1 + N5 * N0, N0, Nv0, Nv1, Rv0, Rm1 + N6 * N0);
+
+                // Sketch application.
+                Mlc_RvtNNvNvRvRv_0(Rm3 + (N4 - 1) * N3, N0, Nv2, Nv3, Rv2, Rm1 + N5 * N0);
+
+                // Orthogonalization.
+                for(natural_t N7 = 0; N7 <= N2; ++N7) {
+                    if(N5 == N7)
+                        continue;
+
+                    Prj_RvtRvRvN_0(Rm1 + N5 * N0, Rm1 + N7 * N0, Rm1 + N5 * N0, N0);
+                }
 
                 // Normalization.
                 Nrz_RvtN_0(Rm1 + N5 * N0, N0);
-
-                // LS matrix.
-                Mlc_RvtNNvNvRvRv_0(Rm2 + N5 * N0, N0, Nv0, Nv1, Rv0, Rm1 + N5 * N0);
             }
 
-            // Truncated Arnoldi, last columns.
-            for(natural_t N5 = N4; N5 < N1; ++N5) {
+            // Zeroing.
+            #pragma omp parallel for
+            for(natural_t N7 = 0; N7 < N0; ++N7)
+                Rm1[(N1 % (N2 + 1)) * N0 + N7] = 0.0;
 
-                // Copy.
-                Cp_RvtRvN_0(Rm1 + N5 * N0, Rm2 + (N5 - 1) * N0, N0);
+            // LS column.
+            Mlc_RvtNNvNvRvRv_0(Rm1 + (N1 % (N2 + 1)) * N0, N0, Nv0, Nv1, Rv0, Rm1 + ((N1 - 1) % (N2 + 1)) * N0);
 
-                // Orthogonalization.
-                for(natural_t N6 = 1; N6 <= N4; ++N6)
-                    Prj_RvtRvRvN_0(Rm1 + N5 * N0, Rm1 + (N5 - N6) * N0, Rm1 + N5 * N0, N0);
+            // Sketch application.
+            Mlc_RvtNNvNvRvRv_0(Rm3 + (N1 - 1) * N3, N0, Nv2, Nv3, Rv2, Rm1 + (N1 % (N2 + 1)) * N0);
 
-                // Normalization.
-                Nrz_RvtN_0(Rm1 + N5 * N0, N0);
-
-                // LS matrix.
-                Mlc_RvtNNvNvRvRv_0(Rm2 + N5 * N0, N0, Nv0, Nv1, Rv0, Rm1 + N5 * N0);
-            }
+            #endif
 
 
             #ifndef NVERBOSE
             T1 = std::chrono::high_resolution_clock::now();
 
+            #ifndef MEMORY_PRIORITY
             std::println("\tArnoldi: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
+            #else
+            std::println("\tArnoldi (1) and sketch application: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
+            #endif
             #endif
 
 
+            #ifndef MEMORY_PRIORITY
             #ifndef NVERBOSE
             T0 = std::chrono::high_resolution_clock::now();
             #endif
@@ -204,6 +288,7 @@ namespace nass {
             T1 = std::chrono::high_resolution_clock::now();
 
             std::println("\tSketch application: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
+            #endif
             #endif
 
 
@@ -226,8 +311,8 @@ namespace nass {
             // Condition number estimate.
             real_t R0 = std::abs(Rm3[0]), R1 = std::abs(Rm3[0]);
 
-            for(natural_t N5 = 1; N5 < N1; ++N5) {
-                const real_t R2 = std::abs(Rm3[N5 * (N3 + 1)]);
+            for(natural_t N4 = 1; N4 < N1; ++N4) {
+                const real_t R2 = std::abs(Rm3[N4 * (N3 + 1)]);
 
                 if(R2 < R0)
                     R0 = R2;
@@ -246,25 +331,25 @@ namespace nass {
             Mqt_RvtRmNN_0(Rv5, Rm4, N3, N1, N1);
 
             // (Reduced) LS problem, backward substitution.
-            for(natural_t N5 = N1; N5 > 0; --N5) {
+            for(natural_t N4 = N1; N4 > 0; --N4) {
                 real_t R2 = 0.0;
 
-                for(natural_t N6 = N5; N6 < N1; ++N6)
-                    R2 += Rm3[N6 * N3 + N5 - 1] * Rv6[N6];
+                for(natural_t N5 = N4; N5 < N1; ++N5)
+                    R2 += Rm3[N5 * N3 + N4 - 1] * Rv6[N5];
 
-                Rv6[N5 - 1] = (Rv5[N5 - 1] - R2) / Rm3[(N5 - 1) * (N3 + 1)];
+                Rv6[N4 - 1] = (Rv5[N4 - 1] - R2) / Rm3[(N4 - 1) * (N3 + 1)];
             }
 
             // Pivoting.
-            for(natural_t N5 = 0; N5 < N1; ++N5)
-                Rv7[Nv4[N5]] = Rv6[N5];
+            for(natural_t N4 = 0; N4 < N1; ++N4)
+                Rv7[Nv4[N4]] = Rv6[N4];
 
             // Residual estimation, Q.
             Mq_RvtRmNN_0(Rv5, Rm4, N3, N1, N1);
 
             // Residual estimation, subtraction.
-            for(natural_t N5 = 0; N5 < N3; ++N5)
-                Rv8[N5] -= Rv5[N5];
+            for(natural_t N4 = 0; N4 < N3; ++N4)
+                Rv8[N4] -= Rv5[N4];
 
             // Residual estimation. [!] Q needs to be truncated.
             const real_t R2 = Nr_RvN_R(Rv8, N3);
@@ -282,14 +367,93 @@ namespace nass {
             #endif
 
 
+            #ifndef MEMORY_PRIORITY
+
             // Solution update.
             Ml_RvtRmRvNN_0(Rvt0, Rm1, Rv7, N0, N1);
+
+            #else 
+
+            // First basis column.
+            Cp_RvtRvN_0(Rm1, Rv4, N0);
+            Nrz_RvtN_0(Rm1, N0);
+
+            // Solution update.
+            #pragma omp parallel for
+            for(natural_t N5 = 0; N5 < N0; ++N5)
+                Rvt0[N5] += Rm1[N5] * Rv7[0];
+
+            // Truncated Arnoldi and solution update, first part.
+            for(natural_t N4 = 1; N4 <= N2; ++N4) {
+
+                // Zeroing.
+                #pragma omp parallel for
+                for(natural_t N5 = 0; N5 < N0; ++N5)
+                    Rm1[N4 * N0 + N5] = 0.0;
+
+                // LS column.
+                Mlc_RvtNNvNvRvRv_0(Rm1 + N4 * N0, N0, Nv0, Nv1, Rv0, Rm1 + (N4 - 1) * N0);
+
+                // Orthogonalization.
+                for(natural_t N5 = 0; N5 < N4; ++N5)
+                    Prj_RvtRvRvN_0(Rm1 + N4 * N0, Rm1 + N5 * N0, Rm1 + N4 * N0, N0);
+
+                // Normalization.
+                Nrz_RvtN_0(Rm1 + N4 * N0, N0);
+
+                // Solution update.
+                const real_t R3 = Rv7[N4];
+
+                #pragma omp parallel for
+                for(natural_t N5 = 0; N5 < N0; ++N5)
+                    Rvt0[N5] += Rm1[N4 * N0 + N5] * R3;
+            }
+
+            // Truncated Arnoldi, second part.
+            for(natural_t N4 = N2 + 1; N4 < N1; ++N4) {
+
+                // Indices.
+                const natural_t N5 = N4 % (N2 + 1);
+                const natural_t N6 = (N4 - 1) % (N2 + 1);
+
+                // Zeroing.
+                #pragma omp parallel for
+                for(natural_t N7 = 0; N7 < N0; ++N7)
+                    Rm1[N5 * N0 + N7] = 0.0;
+
+                // LS column.
+                Mlc_RvtNNvNvRvRv_0(Rm1 + N5 * N0, N0, Nv0, Nv1, Rv0, Rm1 + N6 * N0);
+
+                // Orthogonalization.
+                for(natural_t N7 = 0; N7 <= N2; ++N7) {
+                    if(N5 == N7)
+                        continue;
+
+                    Prj_RvtRvRvN_0(Rm1 + N5 * N0, Rm1 + N7 * N0, Rm1 + N5 * N0, N0);
+                }
+
+                // Normalization.
+                Nrz_RvtN_0(Rm1 + N5 * N0, N0);
+
+                // Solution update.
+                const real_t R3 = Rv7[N4];
+
+                #pragma omp parallel for
+                for(natural_t N7 = 0; N7 < N0; ++N7)
+                    Rvt0[N7] += Rm1[N5 * N0 + N7] * R3;
+            }
+
+            #endif
 
 
             #ifndef NVERBOSE
             T1 = std::chrono::high_resolution_clock::now();
 
+            #ifndef MEMORY_PRIORITY
             std::println("\tSolution update: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
+            #else
+            std::println("\tArnoldi (2) and solution update: {}", std::chrono::duration_cast<std::chrono::milliseconds>(T1 - T0));
+            #endif
             #endif
 
 
@@ -308,7 +472,11 @@ namespace nass {
             #endif
 
             delete[] Rm1;
+
+            #ifndef MEMORY_PRIORITY
             delete[] Rm2;
+            #endif
+
             delete[] Rm3;
             delete[] Rm4; delete[] Nv4;
             delete[] Rv3;
